0f881b2546895993141ee69de7e784d0c2b932d3
Paul Rodriguez <paul@prdesign.work>
2017-10-19 12:36:04 -0600

    db adjustments

--- knexfile.js
+++ knexfile.js
@@ -4,40 +4,14 @@ module.exports = {
 
 
   development: {
-    client: 'pg',
-    connection: {
-      database: 'g64-secrets'
-    }
-  },
-
-  staging: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    connection: 'postgres://localhost/g64-secrets',
   },
 
   production: {
-    client: 'pg',
-    connection: {
-      database: 'DATABASE_URL',
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL
+
+  },
 
 };
